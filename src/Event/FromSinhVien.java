db72800a629c632911c468b99a976fcbf13cd328
Admin <tranquockhanh0506@gmail.com>
2020-10-22 20:47:55 +0700

    da sua xong

--- src/Event/FromSinhVien.java
+++ src/Event/FromSinhVien.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -220,6 +220,8 @@ public class FromSinhVien extends javax.swing.JFrame implements ActionListener, 
                 tableRecords.set(iDongDaChon, record);
                 table.setModel(new DefaultTableModel(tableRecords, tableTitle));
 //            JOptionPane.showMessageDialog(rootPane, "Cập nhật xong");
+// da sua xong
+
             }        // TODO add your handling code here:
     }//GEN-LAST:event_suaActionPerformed
 
